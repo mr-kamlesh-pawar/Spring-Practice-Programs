ed97ed70b398c7b26bf5ff6253fdec77ba72c50b
Kamlesh Pawar <kamleshpawar5278@gmail.com>
2025-06-24 23:25:22 +0530

    jpa

---
+++ springboot_jpa_01/src/main/java/com/kp/springboot_jpa_01/SpringbootJpa01Application.java
@@ -0,0 +1,13 @@
+package com.kp.springboot_jpa_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpa01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpa01Application.class, args);
+	}
+
+}
